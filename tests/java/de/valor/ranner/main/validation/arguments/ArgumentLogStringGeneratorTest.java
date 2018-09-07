2893947a4ef3a8520a4d19970e0dad67fe60006b
valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-09-07 12:43:19 +0200

    Adding Tests for ArgumentLogStringGenerator

---
+++ tests/java/de/valor/ranner/main/validation/arguments/ArgumentLogStringGeneratorTest.java
@@ -0,0 +1,50 @@
+package de.valor.ranner.main.validation.arguments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgumentLogStringGeneratorTest {
+
+    @Test
+    void getTypeError() {
+        String expected = "Given argument is not of type: String";
+        String actual = ArgumentLogStringGenerator.getTypeError("String", null);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getNotExistsError() {
+        String expected = "The given argument is null or empty.";
+        String actual = ArgumentLogStringGenerator.getNotExistsError(null);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getNullError() {
+        String expected = "The given argument is null.";
+        String actual = ArgumentLogStringGenerator.getNullError(null);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getEmptyError() {
+        String expected = "The given argument is empty.";
+        String actual = ArgumentLogStringGenerator.getEmptyError(null);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getValidateExistResultTrue() {
+        String expected = "The given argument does exist and is a valid type.";
+        String actual = ArgumentLogStringGenerator.getValidateExistResult(true, null);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getValidateExistResultFalse() {
+        String expected = "The given argument does not exist or is not a valid type!";
+        String actual = ArgumentLogStringGenerator.getValidateExistResult(false, null);
+        assertEquals(expected, actual);
+    }
+}
---
+++ tests/java/de/valor/ranner/main/validation/arguments/ArgumentLogStringGeneratorWithNameTest.java
@@ -0,0 +1,50 @@
+package de.valor.ranner.main.validation.arguments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArgumentLogStringGeneratorWithNameTest {
+
+    @Test
+    void getTypeError() {
+        String expected = "Given argument \"someParameter\" is not of type: String";
+        String actual = ArgumentLogStringGenerator.getTypeError("String", "someParameter");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getNotExistsError() {
+        String expected = "The given argument \"test\" is null or empty.";
+        String actual = ArgumentLogStringGenerator.getNotExistsError("test");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getNullError() {
+        String expected = "The given argument \"unicorn\" is null.";
+        String actual = ArgumentLogStringGenerator.getNullError("unicorn");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getEmptyError() {
+        String expected = "The given argument \"bunny\" is empty.";
+        String actual = ArgumentLogStringGenerator.getEmptyError("bunny");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getValidateExistResultTrue() {
+        String expected = "The given argument \"mouse\" does exist and is a valid type.";
+        String actual = ArgumentLogStringGenerator.getValidateExistResult(true, "mouse");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getValidateExistResultFalse() {
+        String expected = "The given argument \"freakyParameter\" does not exist or is not a valid type!";
+        String actual = ArgumentLogStringGenerator.getValidateExistResult(false, "freakyParameter");
+        assertEquals(expected, actual);
+    }
+}
