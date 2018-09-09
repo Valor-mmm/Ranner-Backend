c7d30851109ca6546b8e07b508eab5b25ce2169d
valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-09-09 19:09:20 +0200

    Cleaning up code smell

--- src/java/de/valor/ranner/main/validation/arguments/ArgumentLogStringGenerator.java
+++ src/java/de/valor/ranner/main/validation/arguments/ArgumentLogStringGenerator.java
@@ -5,47 +5,47 @@ package de.valor.ranner.main.validation.arguments;
  */
 class ArgumentLogStringGenerator {
 
-    private static final String typeError = "Given argument is not of type: ";
-    private static final String typeErrorTemplate = "Given argument \"%s\" is not of type: ";
+    private static final String TYPE_ERROR = "Given argument is not of type: ";
+    private static final String TYPE_ERROR_TEMPLATE = "Given argument \"%s\" is not of type: ";
 
-    private static final String notExistsError = "The given argument is null or empty.";
-    private static final String notExistsErrorTemplate = "The given argument \"%s\" is null or empty.";
+    private static final String NOT_EXISTS_ERROR = "The given argument is null or empty.";
+    private static final String NOT_EXISTS_ERROR_TEMPLATE = "The given argument \"%s\" is null or empty.";
 
-    private static final String nullError = "The given argument is null.";
-    private static final String nullErrorTemplate = "The given argument \"%s\" is null.";
+    private static final String NULL_ERROR = "The given argument is null.";
+    private static final String NULL_ERROR_TEMPLATE = "The given argument \"%s\" is null.";
 
-    private static final String emptyError = "The given argument is empty.";
-    private static final String emptyErrorTemplate = "The given argument \"%s\" is empty.";
+    private static final String EMPTY_ERROR = "The given argument is empty.";
+    private static final String EMPTY_ERROR_TEMPLATE = "The given argument \"%s\" is empty.";
 
-    private static final String validateExistResultPositive = "The given argument does exist and is a valid type.";
-    private static final String validateExistResultPositiveTemplate = "The given argument \"%s\" does exist and is a valid type.";
+    private static final String VALIDATE_EXIST_RESULT_POSITIVE = "The given argument does exist and is a valid type.";
+    private static final String VALIDATE_EXIST_RESULT_POSITIVE_TEMPLATE = "The given argument \"%s\" does exist and is a valid type.";
 
-    private static final String validateExistResultNegative = "The given argument does not exist or is not a valid type!";
-    private static final String validateExistResultNegativeTemplate = "The given argument \"%s\" does not exist or is not a valid type!";
+    private static final String VALIDATE_EXIST_RESULT_NEGATIVE = "The given argument does not exist or is not a valid type!";
+    private static final String VALIDATE_EXIST_RESULT_NEGATIVE_TEMPLATE = "The given argument \"%s\" does not exist or is not a valid type!";
 
 
     static String getTypeError(String type, String argumentName) {
-        return composeValidationResult(typeError, typeErrorTemplate, argumentName) + type;
+        return composeValidationResult(TYPE_ERROR, TYPE_ERROR_TEMPLATE, argumentName) + type;
     }
 
     static String getNotExistsError(String argumentName) {
-        return composeValidationResult(notExistsError, notExistsErrorTemplate, argumentName);
+        return composeValidationResult(NOT_EXISTS_ERROR, NOT_EXISTS_ERROR_TEMPLATE, argumentName);
     }
 
     static String getNullError(String argumentName) {
-        return composeValidationResult(nullError, nullErrorTemplate, argumentName);
+        return composeValidationResult(NULL_ERROR, NULL_ERROR_TEMPLATE, argumentName);
     }
 
     static String getEmptyError(String argumentName) {
-        return composeValidationResult(emptyError, emptyErrorTemplate, argumentName);
+        return composeValidationResult(EMPTY_ERROR, EMPTY_ERROR_TEMPLATE, argumentName);
     }
 
     static String getValidateExistResult(boolean valid, String argumentName) {
         String result;
         if (valid) {
-            result = composeValidationResult(validateExistResultPositive, validateExistResultPositiveTemplate, argumentName);
+            result = composeValidationResult(VALIDATE_EXIST_RESULT_POSITIVE, VALIDATE_EXIST_RESULT_POSITIVE_TEMPLATE, argumentName);
         } else {
-            result = composeValidationResult(validateExistResultNegative, validateExistResultNegativeTemplate, argumentName);
+            result = composeValidationResult(VALIDATE_EXIST_RESULT_NEGATIVE, VALIDATE_EXIST_RESULT_NEGATIVE_TEMPLATE, argumentName);
         }
         return result;
     }
